e319f0535fe394a6ddfa48f190ad8f7b577a3149
suryabk <surya27bk@gmail.com>
2024-01-01 21:48:43 +0700

    install mantine library

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <MantineProvider>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </MantineProvider>
   )
 }
 
